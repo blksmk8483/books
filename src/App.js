514116196a8a4363151d0c5c530a565665f02e12
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-02 13:37:05 -0600

    added book function

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        
-
+        const updatedBooks = [
+            ...books,
+            { id: 123, title: title }
+        ];
+        setBooks(updatedBooks);
     };
 
     return (
     <div>
+        {books.length}
         <BookCreate onCreate={createBook}/>
     </div>
     );
