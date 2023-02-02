a90c61e4f4be8f89879e13bbc682fff49294dcb4
Brandon Knight <knightbrandon1984@gmail.com>
2023-02-02 16:07:26 -0600

    added delete button for books

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,18 @@
+function BookShow({ book, onDelete }) {
+    const handleClick = () => {
+        onDelete(book.id);
+    };
 
-
-function BookShow({book}) {
-    return <div className="book-show">{book.title}</div>
-}
+    return (
+        <div className="book-show">
+            {book.title}
+            <div className="actions">
+                <button className="delete" onClick={handleClick}>
+                    Delete
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default BookShow;
